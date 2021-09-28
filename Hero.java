53cbcb905332be37b07ada61da3fcdc4dee25c88
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-09-28 03:20:57 -0600

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -2,21 +2,39 @@ import java.util.ArrayList;
 
 public class Hero extends Combatiente{
 
-    ArrayList<Item> bag = new ArrayList<Item>();
-    
-    public Hero(String nombre, int vida, int pts_atack, String wd_i, String wd_f) {
-        super(nombre, vida, pts_atack, wd_i, wd_f);
+    int max_size = 3;
+    protected ArrayList<Item> bag = new ArrayList<Item>();
+
+
+    public Hero(String nombre) {
+        super(nombre);
     }
 
+    
+    public void setSize(int num){// no máximo de elementos. 
+        this.max_size = num;
+    }
 
     public void addItem(Item e){
-        bag.add(e);
-        // Mostrar un mensaje de confirmación?
+        if (bag.size()<max_size){
+            bag.add(e);
+        }else{
+            // LLAMAR MÉTODO DE LA VISTA PARA DECIR QUE NO SE PUDO. SE PUEDE PONER TRY-CATCH. 
+        }
+        
     }
 
     public void useItem(Item e){
         bag.remove(e);
-        // Mostrar un mensaje de confirmación?
+    }
+
+    public ArrayList<String> insideBag(ArrayList<Item> list){
+        ArrayList<String> nom_items = new ArrayList<String>();
+        for (int i = 0; i < list.size(); i++){
+            nom_items.add(list.get(i).getName());
+        }
+
+        return nom_items;
     }
 
     
