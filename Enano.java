9946165f8ed534bbe5870173c25d3fb400f879cd
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-09-28 03:24:34 -0600

    new properties of the enemies

--- Enano.java
+++ Enano.java
@@ -3,6 +3,7 @@ public class Enano extends Npc{
     public Enano(String nombre) {
         super(nombre);
         setVida(5);
+        setAttack(1);
     }
     
 }
--- Esqueleto.java
+++ Esqueleto.java
@@ -3,6 +3,7 @@ public class Esqueleto extends Npc {
     public Esqueleto(String nombre) {
         super(nombre);
         setVida(3);
+        setAttack(3);
     }
     
 }
