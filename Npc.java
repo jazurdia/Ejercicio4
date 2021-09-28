d8620cab9cacfac7c4f3c1f8326977138874fb4f
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-09-28 03:21:10 -0600

    Create Npc.java

---
+++ Npc.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Npc extends Combatiente{
+
+    public Npc(String nombre) {
+        super(nombre);
+    }
+
+    public int specialAttack(){
+        Random rand = new Random();
+        int bonus = pts_attack * rand.nextInt(2);
+        
+        return bonus;
+    }
+
+
+    
+    
+}
