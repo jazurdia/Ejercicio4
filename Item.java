c72ce5a9395db7ca00c8dd0c07469e576a40a31e
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-09-28 03:21:06 -0600

    Update Item.java

---
+++ Item.java
@@ -0,0 +1,19 @@
+public class Item {
+    
+    int effect;
+    String name;
+    
+    public Item(String name, int effect){// Builder
+        this.name = name;
+        this.effect = effect;
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+
+    public int effect(){
+        return effect;
+    }
+}
